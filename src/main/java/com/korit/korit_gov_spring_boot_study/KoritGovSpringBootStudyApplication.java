cc12e37c77dbf7fca4a0affbe567f2889678e0c8
KimCoal <121536823+KimCoal@users.noreply.github.com>
2025-11-18 11:31:38 +0900

    Initial commit

---
+++ src/main/java/com/korit/korit_gov_spring_boot_study/KoritGovSpringBootStudyApplication.java
@@ -0,0 +1,12 @@
+package com.korit.korit_gov_spring_boot_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoritGovSpringBootStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoritGovSpringBootStudyApplication.class, args);
+	}
+}
---
+++ src/main/java/com/korit/korit_gov_spring_boot_study/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.korit.korit_gov_spring_boot_study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// SSR > 즉 서버쪽에서 웹페이지를 핸더링해서 반환하는 방식
+@Controller
+public class MainController {
+
+    // 동적인 요소가 없는 정적 웹페이지
+    @GetMapping("/main")
+    public String getMain() {
+        return "main.html";
+    }
+
+    // SSR에 동적을 추가하려면 Thymleaf를 적용
+    // 서버에서 HTML을 렌더링할떄, 자바 데이터를 끼워 넣을 수 있게 해주는 템플릿 엔진
+}
